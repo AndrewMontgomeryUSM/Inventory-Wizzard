5bd7c3d9d842d6d71f9d694464b6290995e81642
AndrewM87 <andrewmontgomery87@icloud.com>
2025-05-12 11:49:56 +0700

    Updates to pantry application

--- pantry.cpp
+++ pantry.cpp
@@ -5,11 +5,11 @@ Features:
 Make inventory/pantry from scratch
 Compare quantity on hand against quantity in inventory
 Create Grocery List when quantities drop below minimum
+Edit item function allows searching single item and
+    a) editing quantity
+    b) editing cost
 
 Yet to implement:
-Create an "Edit inventory" function that:
-    a) allows searching item by name
-    b) allows editing of quantity and cost
 Create a "Receive Groceries" Function that:
     a) steps through grocery list or receipt
     b) "receives" items purchased
@@ -41,6 +41,9 @@ void fillInventory();
 Provision* loadInventory(int&);
 int binCounter();
 void groceryList(int, Provision*);
+bool inList(int, Provision*, std::string);
+void editQuantity(int, Provision*);
+void editPrice(int, Provision*);
 
 int main()
 {
@@ -48,12 +51,16 @@ int main()
     Provision* pantry = loadInventory(totalBins);
 
     int choice;
+    std::string grocery;   
 
     // Menu options; added as functions are added.
     cout << "*****Inventory Management System*****\n";
     cout << "_____________________________________\n";
-    cout << "******Enter 1 to make inventory******\n";
+    cout << "*****Enter 1 to make inventory*******\n";
     cout << "*****Enter 2 to make grocery list****\n";
+    cout << "*****Enter 3 to search for an item***\n";
+    cout << "*****Enter 4 to adjust actual quantity\n";
+    cout << "*****Enter 5 to adjust actual price**\n";
     cin >> choice;
 
     switch(choice)
@@ -63,6 +70,22 @@ int main()
             break;
         case 2: "Completing Grocery List:\n";
             groceryList(totalBins, pantry);
+            break;
+        case 3: "Search for item\n"; 
+            cout << "Item Name: ";
+                cin.ignore();
+                getline(cin, grocery);
+            if (inList(totalBins, pantry, grocery) == true)
+                cout << "Item in stock:\n";
+            else
+                cout << "Item Not Found\n";
+            break;
+        case 4: "Adjusting Quantity on Hand:\n";
+            editQuantity(totalBins, pantry);
+            groceryList(totalBins, pantry);
+            break;
+        case 5: "Adjusting Unit Cost:\n";
+            editPrice(totalBins, pantry);
             break;
         default:
             cout << "Invalid Selection!\n";
@@ -147,6 +170,7 @@ Provision* loadInventory(int& totalBins)
     totalBins = binCounter();
     std::string line;
 
+    // Creates the file if it doesn't exist, replaces it if it does.
     std::ifstream inFile("pantry.csv");
 
     if (!inFile)
@@ -160,6 +184,7 @@ Provision* loadInventory(int& totalBins)
 
     Provision* pantry = new Provision[totalBins];
 
+    // I had an easier time going into tempVariables than straight into the object
     std::string tempName;
     int tempQuant;
     double tempCost;
@@ -246,8 +271,6 @@ void groceryList(int totalBins, Provision* pantryList)
             shoppingList[shoppingCount].setCost(pantryList[count].getCost());
             shoppingCount++;
         }
-        else
-            cout << pantryList[count].getName() << ": Inventory OK" << endl;
 
     }
 
@@ -269,11 +292,133 @@ void groceryList(int totalBins, Provision* pantryList)
     outFile << "\n";
     outFile << "*********************************\n";
     outFile << "Estimated Price for Groceries: \n" << setw(12) << right
-        << setprecision(2) << showpoint << fixed << "$" <<(estimatedCost * 2) << endl;
+        << setprecision(2) << showpoint << fixed << "$" <<(estimatedCost * MINIMUM_INVENTORY) << endl;
 
     cout << "Grocery List Created!" << endl;
 
     delete[] shoppingList;
     shoppingList = nullptr;
     outFile.close();
+}
+
+/*
+-----------------------------------------------------------------------------
+Allows the user to type in the name of an item, then searchs (linearly) the
+array for the item name that matches selection and returns a true or false 
+value to indicate that it is in stock and there are more than 0 units on hand
+-----------------------------------------------------------------------------
+*/
+
+bool inList(int totalBins, Provision* pantryList, std::string item)
+{
+    std::string grocery;
+    
+    bool inStock = false;
+
+    grocery = item;
+
+    for (int count = 0; count < totalBins; count++)
+    {
+        if(grocery == pantryList[count].getName() && pantryList[count].getQuant() > 0)
+        {
+            inStock = true;
+        }
+    }
+
+    return inStock;
+}
+
+/*
+-----------------------------------------------------------------------------
+Allows editing of quantity on hand then reruns groceryList() function to
+catch any items that dropped below the minimum and add them to the list
+
+Currently, allows editing of a single item at a time. Must run multiple
+time in order to receive a grocery order or do an inventory.
+-----------------------------------------------------------------------------
+*/
+void editQuantity(int totalBins, Provision* pantryList)
+{
+    std::string grocery;
+    int actualQuantity;
+    std::ofstream outFile("pantry.csv");
+
+    cout << "Item Name: ";
+    cin.ignore();
+    getline(cin, grocery);
+
+    if (inList(totalBins, pantryList, grocery) != true)
+    {    
+        cout << "Item not in Inventory\n";
+        return;
+    }  
+
+    cout << "Actual Quantity on Hand: ";
+    cin >> actualQuantity;
+
+    if (inList(totalBins, pantryList, grocery) != true)
+        cout << "Item not in Inventory\n";
+
+    // Checks quantity on hand for quantity in object array
+    for (int count = 0; count < totalBins; count++)
+    {
+        if (grocery == pantryList[count].getName() && pantryList[count].getQuant() != actualQuantity)
+        {    
+            pantryList[count].setQuant(actualQuantity);
+            cout << "Quantity changed successfully\n";
+        }
+        else if (grocery == pantryList[count].getName() && pantryList[count].getQuant() == actualQuantity)
+            cout << "Quantity in system matches actual quantity on hand\n";
+    }
+    
+
+    // Saves dynamic array to a .csv file for permanence
+    for (int count = 0; count < totalBins; count++)
+    {
+        outFile << pantryList[count].getName() << ","
+        << pantryList[count].getQuant() << ","
+        << pantryList[count].getCost() << "\n";
+    }
+}
+
+void editPrice(int totalBins, Provision* pantryList)
+{
+    std::string grocery;
+    float actualPrice;
+    std::ofstream outFile("pantry.csv");
+
+
+    cout << "Item Name: ";
+    cin.ignore();
+    getline(cin, grocery);
+
+    if (inList(totalBins, pantryList, grocery) != true)
+    {    
+        cout << "Item not in Inventory\n";
+        return;
+    }  
+
+    cout << "Actual Cost of Item: ";
+    cin >> actualPrice;
+
+    // Checks quantity on hand for quantity in object array
+    for (int count = 0; count < totalBins; count++)
+    {
+        if (grocery == pantryList[count].getName() && pantryList[count].getCost() != actualPrice)
+        {    
+            pantryList[count].setCost(actualPrice);
+            cout << "Cost changed successfully\n";
+        }
+        else if (grocery == pantryList[count].getName() && pantryList[count].getCost() == actualPrice)
+            cout << "Cost in system matches actual quantity on hand\n";
+        
+    }
+
+    // Saves dynamic array to a .csv file for permanence
+    for (int count = 0; count < totalBins; count++)
+    {
+        outFile << pantryList[count].getName() << ","
+        << pantryList[count].getQuant() << ","
+        << pantryList[count].getCost() << "\n";
+    }
 }
